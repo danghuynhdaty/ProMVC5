16193bc1eab50af99949fd4539897707c3f56108
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2018-03-11 11:33:00 +0700

    Hoàn thành chương 06

---
+++ Chuong06/EssentialTools/EssentialTools.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssentialTools.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DHDY")]
+[assembly: AssemblyProduct("EssentialTools.Tests")]
+[assembly: AssemblyCopyright("Copyright © DHDY 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35c70717-d3d2-483f-9249-ead0700db19a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Chuong06/EssentialTools/EssentialTools/Models/Discount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal discountSize;
+
+        public DefaultDiscountHelper(decimal discountSizeParam)
+        {
+            this.discountSize = discountSizeParam;
+        }
+
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize / 100m * totalParam));
+        }
+    }
+}
