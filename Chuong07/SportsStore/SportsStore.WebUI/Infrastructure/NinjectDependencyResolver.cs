a1f6ccce1473d6fff0d82406b96a65e2171bc9ef
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2018-03-11 19:03:19 +0700

    Hoàn thành chương 07

---
+++ Chuong07/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
